1e0ce801b8ee2b6c97780decf6a261bb956ecb4f
Steven <stevenawillets@gmail.com>
2021-01-28 12:40:14 -0500

    Update app.js
    Abstracted out validation

--- app.js
+++ app.js
@@ -86,9 +86,9 @@ app.listen(port, () => {
     return openAdjNode;
   };
 
-  function intializeGame(){
+  function intializeGame(height, width){
     gameState.turn = 1;
-    gameState.openNodeArray = createGrid(4,4);
+    gameState.openNodeArray = createGrid(height,width);
     gameState.storedNode = {};
     gameState.endNodeArray = [];
     gameState.diagonalArray = [];
@@ -109,64 +109,82 @@ app.listen(port, () => {
       }
   };
 
-  function lineEnd(node, player){
+  function validateMulti(diff, xDiff, yDiff){
+    let tempCoords = [];
+    let tempX = gameState.storedNode.x;
+    let tempY = gameState.storedNode.y;
+    let nodesOpen = true;
+
+    for (let i = 1; i < diff;) {
+        tempX = tempX + Math.sign(xDiff);
+        tempY = tempY + Math.sign(yDiff);
+        tempCoords.push({x:tempX, y:tempY});
+        i++;
+    };
+
+    tempCoords.forEach(function(e){
+        if (matchCoords(e, gameState.openNodeArray) === -1) nodesOpen = false;
+    }); 
+    if (nodesOpen){
+        tempCoords.forEach(function(e){
+            deleteNode(e, gameState.openNodeArray);
+        });
+        return true;         
+    } else {
+        return false;
+    };
+  };
+
+  function validateDiagonal(node){
     let validNode = true;
+    let tempArray = [node.x, node.y, gameState.storedNode.x, gameState.storedNode.y];
+    gameState.diagonalArray.forEach(function(e){
+        if (segment_intersection(...tempArray, ...e)) validNode = false;
+    });
+    if (validNode) {
+        gameState.diagonalArray.push(tempArray);
+        return true;
+    } else {
+        return false;
+    }
+  };
 
-    //Check if its an open node
-    if (matchCoords(node,gameState.openNodeArray) !== -1){
-        
-        let xDiff = node.x - gameState.storedNode.x;
-        let yDiff = node.y - gameState.storedNode.y;
-        let xDiffAbs = Math.abs(xDiff);
-        let yDiffAbs = Math.abs(yDiff);
-        let diff = Math.max(xDiffAbs, yDiffAbs);
-        let isDiagonal = xDiffAbs === yDiffAbs;
+  function moveValidation(node){
+    let unusedNode = true;
+    let validDirection = true;
+    let xDiff = node.x - gameState.storedNode.x;
+    let yDiff = node.y - gameState.storedNode.y;
+    let xDiffAbs = Math.abs(xDiff);
+    let yDiffAbs = Math.abs(yDiff);
+    let diff = Math.max(xDiffAbs, yDiffAbs);
+    let isDiagonal = xDiffAbs === yDiffAbs;
 
+    //Check if clicked node is used
+    if (matchCoords(node,gameState.openNodeArray) === -1){
+        unusedNode = false;
+    } else {
         //Verify node is horizontal, vertical, or 45deg relative to start node
         if (xDiff === 0 || yDiff === 0 || isDiagonal) {
             // Special validation for diagonal lines
             if (isDiagonal) {
-                //Check if intersects any existing diagonal lines
-                let tempArray = [node.x, node.y, gameState.storedNode.x, gameState.storedNode.y];
-                gameState.diagonalArray.forEach(function(e){
-                    if (segment_intersection(...tempArray, ...e)) validNode = false;
-                });
-                if (validNode) gameState.diagonalArray.push(tempArray);
-            };
-
-            // Special validation for multi-node lines
-            if (diff > 1) {
-                let tempCoords = [];
-                let tempX = gameState.storedNode.x;
-                let tempY = gameState.storedNode.y;
-                let nodesOpen = true;
-
-                for (let i = 1; i < diff;) {
-                    tempX = tempX + Math.sign(xDiff);
-                    tempY = tempY + Math.sign(yDiff);
-                    tempCoords.push({x:tempX, y:tempY});
-                    i++;
-                };
-
-                tempCoords.forEach(function(e){
-                    if (matchCoords(e, gameState.openNodeArray) === -1) nodesOpen = false;
-                }); 
-                if (nodesOpen){
-                    tempCoords.forEach(function(e){
-                        deleteNode(e, gameState.openNodeArray);
-                    });                
-                } else {
-                    validNode = false;
-                };
+                validDirection = validateDiagonal(node);
             };
         } else {
-            validNode = false;
+            validDirection = false;
         };
-    } else {
-        validNode = false;
+
+        //Check and manage hopped nodes for multi-node lines
+        if (diff > 1) {
+            unusedNode = validateMulti(diff, xDiff, yDiff);
+        };
     };
+    return unusedNode && validDirection;
+  };
+
+  function lineEnd(node, player){
+    let validMove = moveValidation(node);
     
-    if (validNode) {
+    if (validMove) {
         player = player === "Player 1" ? "Player 2" : "Player 1";
 
         //Update arrays
@@ -187,20 +205,22 @@ app.listen(port, () => {
             response.body.heading = player;
             response.body.message = `Awaiting ${player}'s move`;
             gameState.turn = gameState.turn + 1;
-        }
+        };
 
         //Response
         response.msg = "VALID_END_NODE";
         response.body.newLine = {"start": gameState.storedNode, "end": node};              
     } else {
-        gameState.openNodeArray.push([gameState.storedNode.x, gameState.storedNode.y]) //Restore initial node
+        //Restore initial node
+        gameState.openNodeArray.push([gameState.storedNode.x, gameState.storedNode.y]); 
 
         //Response
         response.msg = "INVALID_END_NODE";
         response.body.message = "Please select an unused node that is adjacent vertically, horizontally, or at a 45° angle!";
-    }
+    };
     gameState.storedNode = [];
-  }
+  };
+
     // Segment Intersection - Modified from https://gist.github.com/gordonwoodhull/50eb65d2f048789f9558
     const eps = 0.0000001;
     function between(a, b, c) {
@@ -243,7 +263,7 @@ app.listen(port, () => {
   // INITIALIZE
   app.get("/initialize", function(req,res){
     // Establish initial game state
-    intializeGame();
+    intializeGame(4,4);
 
     res.json({
         "msg": "INITIALIZE",
